b5081863e9efe0c3f87934369e3448b5b8b8ca8a
nur303030 <nur303030a@gmail.com>
2020-02-27 16:11:03 -0500

    first

---
+++ src/main/java/com/stacksimplify/restservice/springbootbuildingblock/SpringbootBuildingblockApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservice.springbootbuildingblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblockApplication.class, args);
+	}
+
+}
